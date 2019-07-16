e580d3c4dfd9a9040e1eee527da769172061ded3
Martin Wudenka <Martin.Wudenka@gmx.de>
2019-07-16 17:02:09 +0200

    reversed: Add wifi ad-hoc + usb OTG + reverse companion

---
+++ stage2/04-reversed/01-run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -e
+
+sed -i "s/rootwait/& modules-load=dwc2,g_ether/" "${ROOTFS_DIR}/boot/cmdline.txt"
+
+echo "
+interface wlan0
+    static ip_address=192.168.4.1/24
+    nohook wpa_supplicant
+" >> "${ROOTFS_DIR}/etc/dhcpcd.conf"
+
+mv "${ROOTFS_DIR}/etc/dnsmasq.conf" "${ROOTFS_DIR}/etc/dnsmasq.conf.orig"
+
+install -v -m 655 files/dnsmasq.conf	"${ROOTFS_DIR}/etc/"
+
+install -v -d					"${ROOTFS_DIR}/etc/hostapd"
+install -v -m 655 files/hostapd.conf	"${ROOTFS_DIR}/etc/hostapd/"
+
+echo "
+ssid=${SSID}
+wpa_passphrase=${WPA_PASSPHRASE}
+" >> "${ROOTFS_DIR}/etc/hostapd/hostapd.conf"
+
+echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" >> "${ROOTFS_DIR}/etc/default/hostapd"
+
+echo "192.168.4.1 reversed.lan" >> "${ROOTFS_DIR}/etc/hosts"
+
+on_chroot <<EOF
+systemctl unmask hostapd
+systemctl enable hostapd
+EOF
+
+on_chroot apt-key add - < files/reversed-ppa.gpg.key
+install -v -m 655 files/reversed.list	"${ROOTFS_DIR}/etc/apt/sources.list.d/"
+on_chroot << EOF
+apt update
+EOF
